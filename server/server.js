32ba48bbbd2f6b47d60d16bca957c3187787cd92
José Manuel Sánchez <jmsanchez@tecnatom.es>
2017-12-27 16:46:00 +0100

    add get/todos/:id to server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID}= require('mongodb');
+
 var app = express();
 
 //middleware
@@ -38,6 +40,23 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) =>{
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch( (e)=>{
+        res.status(400).send();
+    });
 });
 
 app.listen(3000, () =>{
