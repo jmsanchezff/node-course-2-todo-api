c029cf7b3061fba8c87e332ece40e6c3782fbbe2
José Manuel Sánchez <jmsanchez@tecnatom.es>
2017-12-28 13:07:11 +0100

    Test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,5 +101,41 @@ describe('GET /todos:id', () => {
         .expect(404)
         .end(done)
     });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should delete an existing doc', (done)=>{
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res)=>{
+            if (err) {
+                return done(err);
+            }
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }). catch((e)=>done(e));
+        });
+    });
+
+    it('should return 404 if not todo found', (done) =>{
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if invalid object id', (done) =>{
+        request(app)
+        .delete(`/todos/123456`)
+        .expect(404)
+        .end(done);
+    });
 
 });
